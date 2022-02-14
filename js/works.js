1779191a602adc35569f77e801cf084789e16707
Dylan Rijnsburger <dylanrijnsburger@gmail.com>
2022-02-15 00:10:38 +0100

    Working hard or hardly workin eh!

---
+++ js/works.js
@@ -0,0 +1,12 @@
+function loadImage() {
+    /**
+     *  images --> Fotos --> [years] --> [images]
+     *
+     *  from 2014 to 2024:
+     *      if there exists a folder name with the same name as the year
+     *          select it
+     *  for every picture in that folder
+     *  create a new tag and inject it into the HTML.
+     */
+}
+
